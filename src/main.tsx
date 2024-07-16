b5eeabd27d02c28f5fe287f2e854bb52126e8f57
wengweijian <sniggiw@qq.com>
2024-07-16 16:39:41 +0800

    feat:init router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
