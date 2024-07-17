356dc421edce009a6a7a03a4c0564e5b18243030
wengweijian <sniggiw@qq.com>
2024-07-17 15:16:23 +0800

    feat:get route.children

--- src/router/autoload.ts
+++ src/router/autoload.ts
@@ -1,21 +1,35 @@
 import { RouteObject } from "react-router-dom";
 
 const layouts = import.meta.glob("../layouts/*.tsx", { eager: true });
+const pages = import.meta.glob("../pages/**/*.tsx", { eager: true });
 
 function getRoutes() {
     const layoutRoutes = [] as RouteObject[];
     Object.entries(layouts).forEach(([path, module]) => {
         const route = getRouteByModule(path, module as { [key: string]: any });
+        route.children = getChildrenRoutes(route);
         layoutRoutes.push(route);
     });
     return layoutRoutes;
 }
 
+function getChildrenRoutes(route: RouteObject) {
+    const routes = [] as RouteObject[];
+    Object.entries(pages).forEach(([path, module]) => {
+        const layoutName = route.path!.split("/").pop();
+        if (path.includes(`../pages/${layoutName}`)) {
+            const route = getRouteByModule(path, module as { [key: string]: any });
+            routes.push(route);
+        }
+    });
+    return routes;
+}
+
 function getRouteByModule(path: string, module: { [key: string]: any }) {
-    const name = path.split("/").pop()?.split(".")[0];
+    const name = path.replace(/.+layouts\/|.+pages\/|\.tsx/gi, "");
     const route = {
         path: `/${name}`,
-        element: module.default,
+        element: module.default(),
     } as RouteObject;
     return route;
 }
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,13 +1,8 @@
 import { RouteObject } from "react-router-dom";
 
-import Admin from "@/layouts/admin";
 import Home from "@/pages/home";
 
 const routes: RouteObject[] = [
-    // {
-    //     path: "/",
-    //     element: <Home />
-    // },
     {
         path: "/",
         element: <Home />,
